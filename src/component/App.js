565d557ba931793fd2d07e5b21f5e91a3ef6e9c6
unknown <jmuskopf@wustl.edu>
2019-01-06 14:10:54 -0600

    trying random stuff

--- src/component/App.js
+++ src/component/App.js
@@ -23,6 +23,9 @@ class App extends React.Component {
       <div className="component-app">
         <Display value={this.state.next || this.state.total || "0"} />
         <ButtonPanel clickHandler={this.handleClick} />
+        <a href="https://github.com/jackmuskopf/react-app" title="github">
+          <img src="dragon.png" class="imgBorder" width="200" height="200" alt="dragon"> 
+        Dragon </a>
       </div>
     );
   }
