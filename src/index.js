03ee5b86a609d6da894f192d9c4d5836e13c8521
unknown <jmuskopf@wustl.edu>
2019-01-06 16:46:56 -0600

    after too much deliberation

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import "./index.css";
 
 ReactDOM.render(
 <div>
-  <nav class="social">
+  <nav className="social">
     <ul>
-      <li><a href="https://github.com/jackmuskopf">Github <i class="fab fa-github"></i></a></li>
-      <li><a href="www.linkedin.com/in/john-muskopf">LinkedIn <i class="fab fa-linkedin"></i></a></li>
+      <li><a href="https://github.com/jackmuskopf">Github <i className="fab fa-github"></i></a></li>
+      <li><a href="www.linkedin.com/in/john-muskopf">LinkedIn <i className="fab fa-linkedin"></i></a></li>
     </ul>
   </nav>
-  <div class="main">
+  <div className="main">
     <p>No, no, it's okay, really</p>
     <h1>you don't have</h1>
     <p>to stay</p>
-    <img src="./assets/dragon.png" class="pic"/>
+    <img src="./assets/dragon.png" className="pic"/>
     <p>
       <a href="./index.html/">These</a> | 
       <a href="index.html">Are just</a> | 
