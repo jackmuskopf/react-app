fdf5b03afcf5269337d1b54c34a935c50f53de21
unknown <jmuskopf@wustl.edu>
2019-01-06 20:56:18 -0600

    fire

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ ReactDOM.render(
     <p>No, no, it's okay, really</p>
     <h1>you don't have</h1>
     <p>to stay</p>
-    <img src="./dragon.png" alt="dragon" className="pic"/>
+    <a href="/src/doom-fire/index.html/">
+      <img src="./dragon.png" alt="dragon" className="pic"/>
+    </a>
     <p>
       <a href="index.html">These</a> | 
       <a href="index.html">Are just</a> | 
